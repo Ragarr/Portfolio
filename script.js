352479229115994b5b391f02a767f8ec93c46ffc
Raúl Aguilar Arroyo <raulagarr@gmail.com>
2024-02-25 14:31:12 +0100

    highliting commands and selecting with arrows

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.querySelector(".terminal").addEventListener("click", function () {
 
 
 
-const comandos = [
+const ALL_COMMANDS = [
     "help",
     "clear",
     "about-me",
@@ -15,14 +15,61 @@ const comandos = [
     "contact",
     "projects",
     "github",
-    
-
 ]
+
+var coincidentCommands = ALL_COMMANDS;
+
+
+var selectedComand = 0;
+
 
 var autocompleteContainer = document.querySelector('.autocomplete-container');
 
-comandos.forEach(function(comando) {
+coincidentCommands.forEach(function(comando) {
   var listItem = document.createElement('li');
   listItem.textContent = comando;
   autocompleteContainer.appendChild(listItem);
 });
+
+// RESALTAR LOS COMANDOS AL PULSAR UP Y DOWN, HACERLO CICLICO
+document.querySelector("#command-input").addEventListener("keydown", function (e) {
+    if (e.key === "ArrowUp") {
+        selectedComand--;
+        if (selectedComand < 0) {
+            selectedComand = coincidentCommands.length - 1;
+        }
+        document.querySelector("#command-input").value = coincidentCommands[selectedComand];
+    } else if (e.key === "ArrowDown") {
+        selectedComand++;
+        if (selectedComand > coincidentCommands.length - 1) {
+            selectedComand = 0;
+        }
+        document.querySelector("#command-input").value = coincidentCommands[selectedComand];
+    }
+    // remove highlight from all commands
+    autocompleteContainer.querySelectorAll('li').forEach(function (command) {
+        command.classList.remove('highlighted');
+    });
+
+    // highlight selected command
+    autocompleteContainer.children[selectedComand].classList.add('highlighted');
+    
+});
+
+// AL ESCRIIR EN EL INPUT, BUSCAR COMANDOS SIMILARES
+document.querySelector("#command-input").addEventListener("input", function (e) {
+    var input = e.target.value;
+    coincidentCommands = ALL_COMMANDS.filter(function (command) {
+        return command.startsWith(input);
+    });
+    selectedComand = 0;
+    autocompleteContainer.innerHTML = "";
+    coincidentCommands.forEach(function(comando) {
+        var listItem = document.createElement('li');
+        listItem.textContent = comando;
+        autocompleteContainer.appendChild(listItem);
+    });
+});
+
+
+// HIGHLIGHT SELECTED COMMAND WHEN SELECTING WITH ARROW KEYS (not on hover)
