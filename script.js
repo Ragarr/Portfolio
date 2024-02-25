b74066ed18f8775fd0e5feba8d93f2a238cf8eb2
Raúl Aguilar Arroyo <raulagarr@gmail.com>
2024-02-25 14:18:33 +0100

    list elements

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-
-
 // SI CLICK EN TERMINAL, FOCO EN INPUT
-document.querySelector('.terminal').addEventListener('click', function() {
-    document.querySelector('#command-input').focus();
-  });
-  
+document.querySelector(".terminal").addEventListener("click", function () {
+    document.querySelector("#command-input").focus();
+});
+
+
+
+const comandos = [
+    "help",
+    "clear",
+    "about-me",
+    "skills",
+    "education",
+    "experience",
+    "contact",
+    "projects",
+    "github",
+    
+
+]
+
+var autocompleteContainer = document.querySelector('.autocomplete-container');
+
+comandos.forEach(function(comando) {
+  var listItem = document.createElement('li');
+  listItem.textContent = comando;
+  autocompleteContainer.appendChild(listItem);
+});
