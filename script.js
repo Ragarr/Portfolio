f0edef8559a4293917fa3015181db24a8d9a74db
Raúl Aguilar Arroyo <raulagarr@gmail.com>
2024-02-25 13:48:53 +0100

    ESQUELETO DE UN TERMINAL

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+// SI CLICK EN TERMINAL, FOCO EN INPUT
+document.querySelector('.terminal').addEventListener('click', function() {
+    document.querySelector('#command-input').focus();
+  });
+  
