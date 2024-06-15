05f3d3a5b2b79552f0e537417b741c26780c448a
Raúl Aguilar Arroyo <raulagarr@gmail.com>
2024-06-15 17:52:27 +0200

    mas circulitos

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
+let rootStyles = getComputedStyle(document.documentElement);
+
+let circleColor = rootStyles.getPropertyValue('--circle-color');
+let backgroundColor = rootStyles.getPropertyValue('--background-color');
+let accentColor = rootStyles.getPropertyValue('--accent-color');
+let textColor = rootStyles.getPropertyValue('--text-color');
+
 
 document.addEventListener('mousemove', function(e) {
     // Variables de configuración
-    const circleColor = 'rgba(115, 133, 152, 0.1)'; // Color de acento con opacidad
-    const backgroundColor = 'rgb(60, 65, 94, 0.2)'; // Color de fondo
-    const innerRadius = '1px'; // Radio del color de acento
-    const outerRadius = '500px'; // Radio total del gradiente
+    const innerRadius = '0'; // Radio del color de acento
+    const outerRadius = '300px'; // Radio total del gradiente
     
     const x = e.clientX; // Obtiene la coordenada X del cursor
     const y = e.clientY; // Obtiene la coordenada Y del cursor
@@ -15,7 +20,11 @@ document.addEventListener('mousemove', function(e) {
     // Calcula el porcentaje de la posición del cursor respecto al total de la ventana
     const xPercent = x / width * 100;
     const yPercent = y / height * 100;
-    // Establece el fondo como un gradiente radial que sigue al cursor
-    document.body.style.background = `radial-gradient(circle at ${xPercent}% 
-    ${yPercent}%, ${circleColor} ${innerRadius}, ${backgroundColor} ${outerRadius})`;
+
+    ligt = document.querySelector('.light');
+
+
+    let new_bg = `radial-gradient(circle at ${xPercent}% ${yPercent}%, ${circleColor} ${innerRadius}, transparent ${outerRadius} )`;
+    
+    ligt.style.background = new_bg;
 });
