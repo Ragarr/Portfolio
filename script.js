36a2b632aea4e63f69bd62bf6b76703651b78777
Raúl Aguilar Arroyo <raulagarr@gmail.com>
2024-06-15 19:16:51 +0200

    hay que corregir el scroll al hacer click en el menu del navegador

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let accentColor = rootStyles.getPropertyValue('--accent-color');
 let textColor = rootStyles.getPropertyValue('--text-color');
 
 
+
+
 document.addEventListener('mousemove', function(e) {
     // Variables de configuración
     const innerRadius = '0'; // Radio del color de acento
@@ -28,3 +30,53 @@ document.addEventListener('mousemove', function(e) {
     
     ligt.style.background = new_bg;
 });
+
+
+nav_links = document.querySelectorAll('.nav-link');
+
+nav_links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        nav_links.forEach(link => {
+            link.classList.remove('selected-index');
+        });
+        link.classList.add('selected-index');
+        
+        // get the a inside the li (nav_link is the li)
+        let target = link.querySelector('a').getAttribute('href');
+        let target_section = document.querySelector(target);
+        let target_section_top = target_section.offsetTop;
+        window.scrollTo({
+            top: target_section_top,
+            behavior: 'smooth'
+        });
+        
+    });
+});
+
+// cuando navegas por las secciones de la página 
+// se cambia el color del link seleccionado correspondiente a la sección actual
+
+window.addEventListener('scroll', function() {
+    let scroll_position = window.scrollY;
+    let sections = document.querySelectorAll('section');
+    let current_section = '';
+
+    sections.forEach(section => {
+        let section_top = section.offsetTop;
+        let section_height = section.clientHeight;
+
+        if (scroll_position >= section_top - section_height / 3) {
+            current_section = section.getAttribute('id');
+        }
+    });
+
+    nav_links.forEach(link => {
+        link.classList.remove('selected-index');
+        if (link.classList.contains(current_section)) {
+            link.classList.add('selected-index');
+        }
+    });
+});
+
+
